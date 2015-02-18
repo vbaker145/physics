93c511bf188a1f627cc6e71e818f53f1b447f69a
Vince Baker <vjb42@drexel.edu>
2015-02-18 16:54:09 -0500

    HW4 submitted

--- quantum1/hw4/p1.m
+++ quantum1/hw4/p1.m
@@ -1,12 +1,15 @@
-clear all; close all
-N=100;
-w0=1;
-w = w0*2*sin( ((1:N).*(pi/2))./(N+1) );
-k_w = [0.1:0.2:2]; %kT/hw0
-E=zeros(1,length(k_w));
-for kidx = 1:length(k_w)
-    kwt = 1/(k_w(kidx));
-    E(kidx)=sum( w./( exp(w.*kwt)-1 ) );
-end
 
-figure; plot(E/N);
+N=100;
+dt = 0.01; %Temperature spacing
+k_w = (dt:dt:1); %kT/hw0
+t=0:0.1:3; %Temperature
+E=zeros(1,length(k_w));
+
+w = 1*2*sin( ((1:N).*(pi/2))./(N+1) );    
+for kidx = 1:length(k_w)
+    E(kidx)=sum( w./(exp(w./k_w(kidx))-1) );
+end
+E=E/N; %Normalize to number of particles
+
+%Calculate the specific heat with numerical derivative
+sheat = diff(E)./0.01;
