d89d996b22b051e1a0aa5c8b22dcd879b3dee94d
Vince Baker <vjb42@drexel.edu>
2015-04-23 21:55:40 -0400

    HW2 complete

--- quantum2/hw2/p4.m
+++ quantum2/hw2/p4.m
@@ -14,14 +14,23 @@ d2 = mean(eRho)-mean(eSigma);
 eOmega = mean(eRho)+mean(d1,d2);
 
 %3D harmonic oscillator potentials, N=3
-states= [3 0 0; 2 1 0; 2 0 1; 1 2 0; 1 0 2; 1 1 1; 0 3 0; 1 2 0; 0 2 1; 0 0 3];
+states= [3 0 0; 
+         2 1 0; 
+         2 0 1; 
+         1 2 0; 
+         1 0 2; 
+         1 1 1; 
+         0 3 0; 
+         0 1 2; 
+         0 2 1; 
+         0 0 3];
 
 bm = 257.33;
 m = [(bm+150) bm bm];
 e = (states(:,1)+1/2)*m(1) + (states(:,2)+1/2)*m(2)+(states(:,3)+1/2)*m(3);
 %figure; plot(e,'x');
 
-npert = 50000; maxPert = 200;
+npert = 50000; maxPert = 30;
 pert = (rand(npert, 3)-1/2) * maxPert;
 mp = repmat(m,npert,1) + pert;
 %pert = -10:0.1:10;
@@ -50,10 +59,5 @@ best10 = (states(:,1)+1/2)*best(1) + (states(:,2)+1/2)*best(2)+(states(:,3)+1/2)
 chi2 = 0;
 for jj=1:9
     chi2 = chi2 + (ep(idx,jj)-actVals(jj))^2/actVar(jj);
-end
-
-chi2_no4 = 0;
-for jj=[1 2 3 5 6 7 8 9]
-    chi2_no4 = chi2_no4 + (ep(idx,jj)-actVals(jj))^2/actVar(jj);
 end
 
