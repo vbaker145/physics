3514e453a330b85a7f74e600f6e8cd186ca2ac64
Vince Baker <vjb42@drexel.edu>
2015-05-22 08:14:57 -0400

    P2 simulation working

---
+++ quantum2/hw5/p1.m
@@ -0,0 +1,21 @@
+clear all;
+
+N=9;
+G=-(N-1)/2:(N-1)/2;
+G = G*2*pi;
+k=0:pi/N:pi;
+
+Vg = [2.8099  0.2685 0.1579 0.0486 -0.0323];
+Vg = [Vg(5:-1:2) Vg];
+Vg = Vg*(1/(2*pi))*2.5;
+for jj=1:N
+   Vmat(jj,:) = circshift(Vg, jj-1, 2 ); 
+end
+ev = [];
+for jj=1:length(k)
+    Kmat = 0.5*(k(jj)+G).^2;
+    Kmat = diag(Kmat)+Vmat;
+    ev(jj,:) = sort(eig(Kmat));   
+end
+
+figure; plot(k, real(ev(:,1:4)))
