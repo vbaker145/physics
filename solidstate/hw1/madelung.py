af840286d3b4c3d6c2480e5e37cb6e5e2608588e
Vince Baker <vjb42@drexel.edu>
2016-10-05 18:10:43 -0400

    HW2 done

---
+++ solidstate/hw1/madelung.py
@@ -0,0 +1,17 @@
+import numpy as np
+import scipy as sp
+from matplotlib import pyplot as pp
+
+lp = np.array(np.zeros(10*10*10))
+dv = np.array(np.zeros(10*10*10))
+print range(3)
+for i in range(10):
+	ii = (i-5)*2
+	for j in range(10):
+		jj = (j-5)*2
+		for k in range(10):
+			kk = (k-5)*2: 
+			lp[idx] = 2.3
+			
+			
+		
